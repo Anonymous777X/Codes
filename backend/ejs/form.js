1611221db48b5741c285f15cee5eb23b99161aa8
Sahil Patyal <sahilpatial522@gmail.com>
2024-09-16 00:23:47 +0530

    new files

--- backend/ejs/form.js
+++ backend/ejs/form.js
@@ -1,16 +1,19 @@
 import express from 'express';
-import path from "path";
-import { fileURLToPath } from "url";
 
 let app = express();
 let port = 8080;
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"/views"));
-
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
 app.listen(port,()=>{
     console.log(`Listening on Port ${port}`);
 });
+app.get("/register",(req,res)=>{
+    let {username, password}=req.query;
+    console.log(req.query);
+    res.send(`Hello & Welcome Back @${username}`);
+})
+app.post("/register",(req,res)=>{
+    let {username, password}=req.body;
+    console.log(req.body);
+    res.send(`Hello & Welcome Back @${username}`);
+})
---
+++ backend/ejs/misc.js
@@ -0,0 +1,7 @@
+function person(value,number){
+    let name = value;
+    let age = number;
+}
+
+let p1 = new person("adam",26);
+console.log("data is",p1.name,p1.age)
